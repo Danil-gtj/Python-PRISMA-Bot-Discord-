ea97a7564cbaf4f887eebdaabeea06f4457f73be
Danil-gtj <84146501+Danil-gtj@users.noreply.github.com>
2024-11-10 15:05:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'TOKEN' : 'MTA2ODg5NjExNzg2Mjk2MTIzMg.GdDhHy.khzfuQuuYQZX15dcu7XCNBe0QcPf_xS_jS9rHo',
+    'TOKEN' : 'YOUR TOKEN',
     'NAME BOT' : 'PRISMA',
     'ID' : 1068896117862961232,
     'PREFIX' : '$'
